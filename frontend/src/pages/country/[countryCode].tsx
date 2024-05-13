c9a83dc030ba8a65cd41f6864cd9e01c5eb885b1
Olivier Gomez <ashitaaaka@gmail.com>
2024-05-13 15:01:11 +0200

    css enhance and add continent to country creation

--- frontend/src/pages/country/[countryCode].tsx
+++ frontend/src/pages/country/[countryCode].tsx
@@ -2,6 +2,7 @@ import {
   useCountryLazyQuery,
   useCountryQuery,
 } from "@/graphql/generated/schema";
+import Link from "next/link";
 import { useRouter } from "next/router";
 import React, { useEffect } from "react";
 
@@ -31,14 +32,20 @@ const countryDetails = () => {
     );
 
   return (
-    <div className="country_card w-full flex flex-col justify-center items-center gap-2">
-      <p className="text-5xl">{countryData?.country?.emoji}</p>
-      <p>
-        Nom : {countryData?.country?.name} ({countryData?.country?.code})
-      </p>
-      {countryData?.country?.continent ? (
-        <p>Continent : {countryData?.country?.continent?.name}</p>
-      ) : null}
+    <div className="w-full flex flex-col items-center gap-8">
+      <div className="country_card flex flex-col items-center gap-2">
+        <p className="text-5xl">{countryData?.country?.emoji}</p>
+        <p>
+          Nom : {countryData?.country?.name} ({countryData?.country?.code})
+        </p>
+        {countryData?.country?.continent ? (
+          <p>Continent : {countryData?.country?.continent?.name}</p>
+        ) : null}
+      </div>
+
+      <Link className="py-1 px-3 bg-pink-600 rounded-sm text-white" href={"/"}>
+        Retour
+      </Link>
     </div>
   );
 };
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,5 +1,6 @@
 "use client";
 import {
+  ListCountriesDocument,
   NewCountryInput,
   useAddCountryMutation,
   useListContinentsQuery,
@@ -9,18 +10,19 @@ import Link from "next/link";
 import React, { useState } from "react";
 
 const index = () => {
-  const [newCountry, setNewCountry] = useState<NewCountryInput>({
+  const initialState = {
     name: "",
     code: "",
     emoji: "",
     continent: null,
-  });
+  };
+  const [newCountry, setNewCountry] = useState<NewCountryInput>(initialState);
 
   const {
+    data: listCountries,
     loading: listCountriesLoading,
     error: listCountriesError,
-    data: listCountries,
-  } = useListCountriesQuery({ fetchPolicy: "no-cache" });
+  } = useListCountriesQuery({ fetchPolicy: "network-only" });
 
   const {
     loading: listContinentsLoading,
@@ -28,7 +30,9 @@ const index = () => {
     data: listContinents,
   } = useListContinentsQuery({ fetchPolicy: "no-cache" });
 
-  const [addCountry, { error: addCountryError }] = useAddCountryMutation();
+  const [addCountry, { error: addCountryError }] = useAddCountryMutation({
+    refetchQueries: [{ query: ListCountriesDocument }],
+  });
 
   if (listCountriesLoading || listContinentsLoading)
     return (
@@ -54,111 +58,116 @@ const index = () => {
   return (
     <div className="flex flex-col gap-8">
       <main className="flex flex-col gap-8">
-        <section className="add_country_container flex justify-center w-full">
+        <section className="add_country_container w-full flex justify-center items-end p-6 lg:p-2">
           <form
-            className="add_country_form flex gap-6 bg-slate-100 rounded-lg p-6 border border-1 flex-wrap"
+            className="add_country_form lg:w-fit w-full flex flex-col gap-4 bg-slate-100 rounded-lg p-6 border border-1 lg:flex-row lg:flex-wrap justify-center"
             onSubmit={(e) => {
               e.preventDefault();
-              console.log("newCountry", newCountry);
-
               if (!newCountry.code || !newCountry.emoji || !newCountry.name)
                 return;
+
               addCountry({
                 variables: { data: newCountry },
                 onCompleted: () => {
-                  setNewCountry({
-                    name: "",
-                    code: "",
-                    emoji: "",
-                    continent: null,
-                  });
+                  setNewCountry(initialState);
                 },
               });
             }}
           >
-            <div className="flex flex-col gap-2">
-              <label htmlFor="name">Name</label>
-              <input
-                type="text"
-                placeholder="Nom du pays"
-                id="name"
-                name="name"
-                className="py-1 px-2"
-                onChange={(e) =>
-                  setNewCountry((prevState) => ({
-                    ...prevState,
-                    name: e.target.value,
-                  }))
-                }
-              />
+            <div className="flex flex-col sm:flex-row gap-6">
+              <div className="flex flex-col gap-2 w-full">
+                <label htmlFor="name">Name</label>
+                <input
+                  value={newCountry.name}
+                  type="text"
+                  placeholder="Nom du pays"
+                  id="name"
+                  name="name"
+                  className="py-2 px-3 h-full rounded-sm"
+                  onChange={(e) =>
+                    setNewCountry((prevState) => ({
+                      ...prevState,
+                      name: e.target.value,
+                    }))
+                  }
+                />
+              </div>
+              <div className="flex flex-col gap-2 w-full">
+                <label htmlFor="emoji">Emoji</label>
+                <input
+                  value={newCountry.emoji}
+                  type="text"
+                  placeholder="Emoji du pays"
+                  id="emoji"
+                  name="emoji"
+                  className="py-2 px-3 h-full rounded-sm"
+                  onChange={(e) =>
+                    setNewCountry((prevState) => ({
+                      ...prevState,
+                      emoji: e.target.value,
+                    }))
+                  }
+                />
+              </div>
             </div>
-            <div className="flex flex-col gap-2">
-              <label htmlFor="emoji">Emoji</label>
-              <input
-                type="text"
-                placeholder="Emoji du pays"
-                id="emoji"
-                name="emoji"
-                className="py-1 px-2"
-                onChange={(e) =>
-                  setNewCountry((prevState) => ({
-                    ...prevState,
-                    emoji: e.target.value,
-                  }))
-                }
-              />
-            </div>
-            <div className="flex flex-col gap-2">
-              <label htmlFor="code">Code</label>
-              <input
-                type="text"
-                placeholder="Code du pays"
-                id="code"
-                name="code"
-                className="py-1 px-2"
-                onChange={(e) =>
-                  setNewCountry((prevState) => ({
-                    ...prevState,
-                    code: e.target.value,
-                  }))
-                }
-              />
-            </div>
-            <div className="flex flex-col gap-2">
-              <label htmlFor="continent">Continent</label>
-              <select
-                id="continent"
-                name="continent"
-                className="py-1 px-2"
-                onChange={(e) =>
-                  setNewCountry((prevState) => ({
-                    ...prevState,
-                    continent: { id: +e.target.value },
-                  }))
-                }
-              >
-                {listContinents?.continents.map((cont) => (
-                  <option key={cont.id} value={cont.id}>
-                    {cont.name}
+            <div className="flex flex-col sm:flex-row gap-6">
+              <div className="flex flex-col gap-2 w-full">
+                <label htmlFor="code">Code</label>
+                <input
+                  value={newCountry.code}
+                  type="text"
+                  placeholder="Code du pays"
+                  id="code"
+                  name="code"
+                  className="py-2 px-3 h-full rounded-sm"
+                  onChange={(e) =>
+                    setNewCountry((prevState) => ({
+                      ...prevState,
+                      code: e.target.value,
+                    }))
+                  }
+                />
+              </div>
+              <div className="flex flex-col gap-2 w-full">
+                <label htmlFor="continent">Continent</label>
+                <select
+                  defaultValue=""
+                  id="continent"
+                  name="continent"
+                  className="py-2 px-3 h-full rounded-sm"
+                  onChange={(e) =>
+                    setNewCountry((prevState) => ({
+                      ...prevState,
+                      continent: { id: +e.target.value },
+                    }))
+                  }
+                >
+                  <option key="initial_value" value="" disabled hidden>
+                    Choisissez un continent
                   </option>
-                ))}
-              </select>
+                  {listContinents?.continents.map((cont) => (
+                    <option key={cont.id} value={cont.id}>
+                      {cont.name}
+                    </option>
+                  ))}
+                </select>
+              </div>
             </div>
             <button
               type="submit"
-              className="bg-pink-600 px-3 text-white rounded-md"
+              className="bg-pink-600 p-3 text-white rounded-md"
             >
               Add
             </button>
           </form>
         </section>
-        <section className="countries_list flex justify-center">
+        <section className="countries_list flex justify-center lg:p-2">
           <div className="flex w-full justify-center flex-wrap gap-4">
             {listCountries &&
               listCountries.countries.map((country) => (
                 <Link
                   href={`country/${country.code}`}
-                  className="country_card flex flex-col items-center border border-1 rounded-md p-4 w-48"
+                  className="country_card flex flex-col items-center border border-1 rounded-md p-4 w-36"
                   key={country.name}
                 >
                   <h3 className="font-normal">{country.name}</h3>
